57a6a1649b4ba0813e5e69ffc8a7e9a60e297dbb
Paul <theassailant@protonmail.com>
2018-05-21 23:02:03 -0400

    Add documentation for Vec4

--- src/vec4.rs
+++ src/vec4.rs
@@ -10,14 +10,18 @@ pub struct Vec4 {
 
 impl Vec4 {
 
+    /// Returns a new Vec4 at [0, 0, 0, 0].
     pub fn new() -> Vec4 {
         Vec4 { x: 0.0, y: 0.0, z: 0.0, w: 0.0 }
     }
 
+    /// Returns a new Vec4 using the given values for x, y, z, and w.
     pub fn new_from_values(x: &f32, y: &f32, z: &f32, w: &f32) -> Vec4 {
         Vec4 { x: *x, y: *y, z: *z, w: *w }
     }
 
+    /// Returns a new Vec4 using the 0, 1, 2, and 3 indices of the given array,
+    /// where [0] -> x, [1] -> y, [2] -> z, and [3] -> w.
     pub fn new_from_array(input: &[f32; 4]) -> Vec4 {
         Vec4 {
             x: input[0],
@@ -27,6 +31,8 @@ impl Vec4 {
         }
     }
 
+    /// Returns an array of the Vec4's x, y, z, and w values where x -> [0],
+    /// y -> [1], z -> [2], and w -> [3].
     pub fn to_array(&self) -> [f32; 4] {
         [ self.x, self.y, self.z, self.w ]
     }
@@ -35,6 +41,7 @@ impl Vec4 {
 impl ops::Add<Vec4> for Vec4 {
     type Output = Vec4;
 
+    /// Adds one Vec4 to another Vec4 and returns a new Vec4.
     fn add(self, other_vec4: Vec4) -> Vec4 {
         Vec4 {
             x: self.x + other_vec4.x,
@@ -47,6 +54,8 @@ impl ops::Add<Vec4> for Vec4 {
 
 impl ops::AddAssign for Vec4 {
 
+    /// Adds one Vec4 to another Vec4 and re-assigns the first Vec4 to the new
+    /// Vec4.
     fn add_assign(&mut self, other_vec4: Vec4) {
         *self = Vec4 {
             x: self.x + other_vec4.x,
@@ -60,6 +69,7 @@ impl ops::AddAssign for Vec4 {
 impl ops::Mul<Vec4> for f32 {
     type Output = Vec4;
 
+    /// Multiplise a scalar value by a Vec4 and returns a Vec4.
     fn mul(self, other_vec4: Vec4) -> Vec4 {
         Vec4 {
             x: self * other_vec4.x,
@@ -73,6 +83,7 @@ impl ops::Mul<Vec4> for f32 {
 impl ops::Mul<Vec4> for Vec4 {
     type Output = f32;
 
+    /// Returns the dot product of 2 Vec4s, which is a scalar floating point.
     fn mul(self, other_vec4: Vec4) -> f32 {
         (self.x * other_vec4.x) + (self.y * other_vec4.y)
         + (self.z * other_vec4.z) + (self.w * other_vec4.w)
@@ -82,6 +93,7 @@ impl ops::Mul<Vec4> for Vec4 {
 impl ops::Neg for Vec4 {
     type Output = Vec4;
 
+    /// Negates the values of Vec4, which in turn negates the Vec4.
     fn neg(self) -> Vec4 {
         Vec4 { x: -self.x, y: -self.y, z: -self.z, w: -self.w }
     }
@@ -90,6 +102,7 @@ impl ops::Neg for Vec4 {
 impl ops::Sub<Vec4> for Vec4 {
     type Output = Vec4;
 
+    /// Subtracts one Vec4 from another Vec4 and returns a new Vec4.
     fn sub(self, other_vec4: Vec4) -> Vec4 {
         Vec4 {
             x: self.x - other_vec4.x,
@@ -102,6 +115,8 @@ impl ops::Sub<Vec4> for Vec4 {
 
 impl ops::SubAssign for Vec4 {
 
+    /// Subtracts one Vec4 from another Vec4 and re-assigns the first Vec4 to
+    /// the new Vec4.
     fn sub_assign(&mut self, other_vec4: Vec4) {
         *self = Vec4 {
             x: self.x - other_vec4.x,
