8360e0d162158eb5437c2a2fc16d3a67395abbb2
Paul <theassailant@protonmail.com>
2018-06-03 23:48:58 -0400

    Set re-exports to inline for structs [ci skip]

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,19 @@
 pub mod vec2;
 pub mod vec3;
 pub mod vec4;
-
-pub use vec2::Vec2;
-pub use vec3::Vec3;
-pub use vec4::Vec4;
-
 pub mod mat2;
 pub mod mat3;
 pub mod mat4;
 
+#[doc(inline)]
+pub use vec2::Vec2;
+#[doc(inline)]
+pub use vec3::Vec3;
+#[doc(inline)]
+pub use vec4::Vec4;
+#[doc(inline)]
 pub use mat2::Mat2;
+#[doc(inline)]
 pub use mat3::Mat3;
+#[doc(inline)]
 pub use mat4::Mat4;
