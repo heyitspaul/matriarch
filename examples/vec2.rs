29ae3e58a076c4be6845b9a93bd2773b3adcb2aa
Paul <theassailant@protonmail.com>
2018-06-04 17:24:27 -0400

    Update Vec2 example

--- examples/vec2.rs
+++ examples/vec2.rs
@@ -3,9 +3,69 @@ extern crate matriarch;
 use matriarch::Vec2;
 
 fn main() {
-    let vec2 = Vec2::new();
-    //=> Vec2 { x: 0.0, y: 0.0 }
-    let new_vec2 = vec2 + Vec2 { x: 1.0, y: 3.0 };
-    println!("{:?}", new_vec2);
-    //=> Vec2 { x: 1.0, y: 3.0 }
+    addition();
+    //=> Vec2 { x: 3.0, y: 5.5 }
+    subtraction();
+    //=> Vec2 { x: 1.0, y: -0.5 }
+    scalar_product();
+    //=> Vec2 { x: 5.0, y: 6.25 }
+    dot_product();
+    //=> 9.5
+    cross_product();
+    //=> Vec3 { x: 0.0, y: 0.0, z: 3.5 }
+}
+
+fn addition() {
+    let vec2 = Vec2 { x: 2.0, y: 2.5 };
+    let other_vec2 = Vec2 { x: 1.0, y: 3.0 };
+    let new_vec2 = vec2 + other_vec2;
+    println!("Addition:");
+    println!("  {:?}", vec2);
+    println!("+ {:?}", other_vec2);
+    println!("-------------------------");
+    println!("= {:?}\n", new_vec2);
+}
+
+fn subtraction() {
+    let vec2 = Vec2 { x: 2.0, y: 2.5 };
+    let other_vec2 = Vec2 { x: 1.0, y: 3.0 };
+    let new_vec2 = vec2 - other_vec2;
+    println!("Subtraction:");
+    println!("  {:?}", vec2);
+    println!("- {:?}", other_vec2);
+    println!("-------------------------");
+    println!("= {:?}\n", new_vec2);
+}
+
+fn scalar_product() {
+    let vec2 = Vec2 { x: 2.0, y: 2.5 };
+    let scalar: f32 = 2.5;
+    let new_vec2 = scalar * vec2;
+    println!("Scalar Product:");
+    println!("  {:?}", scalar);
+    println!("* {:?}", vec2);
+    println!("-------------------------");
+    println!("= {:?}\n", new_vec2);
+}
+
+fn dot_product() {
+    let vec2 = Vec2 { x: 2.0, y: 2.5 };
+    let other_vec2 = Vec2 { x: 1.0, y: 3.0 };
+    let new_vec2 = vec2 * other_vec2;
+    println!("Dot Product:");
+    println!("  {:?}", vec2);
+    println!("* {:?}", other_vec2);
+    println!("-------------------------");
+    println!("= {:?}\n", new_vec2);
+}
+
+fn cross_product() {
+    let vec2 = Vec2 { x: 2.0, y: 2.5 };
+    let other_vec2 = Vec2 { x: 1.0, y: 3.0 };
+    let new_vec3 = vec2.cross_product(&other_vec2);
+    println!("Cross Product:");
+    println!("  {:?}", vec2);
+    println!("× {:?}", other_vec2);
+    println!("---------------------------------");
+    println!("= {:?}\n", new_vec3);
 }
