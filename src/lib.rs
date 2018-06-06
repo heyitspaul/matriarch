caee8f01da2ace4685b47d5de46b68a877d18e33
Paul <theassailant@protonmail.com>
2018-06-06 14:37:28 -0400

    Re-arrange mods and pub uses [skip ci]
    Mods don't have to be public in order to be publically exported by a pub use, which makes the lib code cleaner and the docs just as clean.

--- src/lib.rs
+++ src/lib.rs
@@ -2,28 +2,16 @@
 //!
 //! Matriarch is a Linear Algebra and Matrix library written in pure Rust.
 
-#[doc(hidden)]
-pub mod mat2;
-#[doc(hidden)]
-pub mod mat3;
-#[doc(hidden)]
-pub mod mat4;
-#[doc(hidden)]
-pub mod vec2;
-#[doc(hidden)]
-pub mod vec3;
-#[doc(hidden)]
-pub mod vec4;
+mod mat2;
+mod mat3;
+mod mat4;
+mod vec2;
+mod vec3;
+mod vec4;
 
-#[doc(inline)]
 pub use mat2::Mat2;
-#[doc(inline)]
 pub use mat3::Mat3;
-#[doc(inline)]
 pub use mat4::Mat4;
-#[doc(inline)]
 pub use vec2::Vec2;
-#[doc(inline)]
 pub use vec3::Vec3;
-#[doc(inline)]
 pub use vec4::Vec4;
